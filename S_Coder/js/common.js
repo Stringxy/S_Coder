a31faef833e7d01d235f3bd6ad1db77f4244fcfe
274343534@qq.com <274343534@qq.com>
2017-07-23 17:51:56 +0800

    0723

---
+++ S_Coder/js/common.js
@@ -0,0 +1,10 @@
+// 获取url中的参数
+function getUrlParam (name) {
+     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if (r!= null) {
+        return unescape(r[2]);
+     }else{
+        return null;
+     }
+}  
